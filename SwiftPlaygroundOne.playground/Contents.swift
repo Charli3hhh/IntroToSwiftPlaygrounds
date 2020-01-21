36a438daaf93cfa6f09ffb690d468d9dc11c6db3
Charli3hhh <59860724+Charli3hhh@users.noreply.github.com>
2020-01-21 10:44:58 -1000

    Completed Swift Playgrounds #1

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -4,14 +4,18 @@ var str = "Hello, playground"
 
 //var myFirstVariable: String
 
-var myFirstVariable = "I've created my first variable"
+var myFirstVariable = "I've created my first variable "
 
 //Declare and assign variable in single line of code
-var mySecondVariable = "and look I added two strings together"
+var mySecondVariable = "and look I added two strings together "
 
 var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 //completed parts 1 to 7
-
-
+var Solo = "Hello My Name "
+var Luke = "is Charles Ombao "
+var Leia = "I am from UHWO "
+var NewHope = Solo + Luke + Leia
+print( NewHope)
+//I have completed part 8
